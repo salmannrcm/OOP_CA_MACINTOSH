cdb66609de46471009d497ebd8cd60a8b35e4fe9
salmannrcm <60485917+salmannrcm@users.noreply.github.com>
2021-04-23 19:06:08 +0100

    Add files via upload

---
+++ OOP_CA_MACINTOSH/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OOP_CA_MACINTOSH.Areas.Identity.Data;
+using OOP_CA_MACINTOSH.Data;
+
+[assembly: HostingStartup(typeof(OOP_CA_MACINTOSH.Areas.Identity.IdentityHostingStartup))]
+namespace OOP_CA_MACINTOSH.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireLowercase = false;
+                    }
+                )
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
